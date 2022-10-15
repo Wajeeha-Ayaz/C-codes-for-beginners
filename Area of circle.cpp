36450a05f6271d76e7d28f9309f9f88a1cae57d4
Wajeeha Ayaz <108749381+Wajeeha-Ayaz@users.noreply.github.com>
2022-10-15 19:16:33 +0500

    Add files via upload

---
+++ Area of circle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+	int radius;
+	float area;
+	cout << "enter radius" << endl;
+	cin >> radius;
+	area = 2 * 3.14 * radius;
+	cout << area << endl;
+	system("pause");
+
+
+
+}
---
+++ Area of rectangle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	int length;
+	int width;
+	float area;
+	cout << "enter length" << endl;
+	cin >> length;
+	cout << "enter width" << endl;
+	cin >>width;
+	area = length*width;
+		cout << area << endl;
+
+	system("pause");
+}
+
